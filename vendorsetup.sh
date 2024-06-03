79a93d4bd75e1c7f55cc6e07648a124c82aec380
LeVin <133874347+TuXarr@users.noreply.github.com>
2024-06-04 00:18:26 +0530

    Update vendorsetup.sh
    Switched to Proton_clang and remove Sepolicy_vndr

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 echo 'Starting to clone stuffs needed for your device'
-echo 'Cloning RMX1971 Vendor tree [1/4]'
+echo 'Cloning RMX1971 Vendor tree [1/3]'
 # Vendor
 rm -rf vendor/realme/RMX1971
 if [ ! -d "vendor/realme/RMX1971" ]; then
@@ -7,7 +7,7 @@ if [ ! -d "vendor/realme/RMX1971" ]; then
     git clone https://gitlab.com/kaderbava/vendor_realme_RMX1971 -b 14 vendor/realme/RMX1971
 fi
 
-echo 'Cloning Kernel tree [2/4]'
+echo 'Cloning Kernel tree [2/3]'
 # Kernel
 rm -rf kernel/realme/sdm710
 if [ ! -d "kernel/realme/sdm710" ]; then
@@ -15,20 +15,12 @@ if [ ! -d "kernel/realme/sdm710" ]; then
     git clone https://github.com/kaderbava/android_kernel_realme_sdm710.git -b 14-r5p kernel/realme/sdm710
 fi
 
-echo 'Cloning Mediatek SEpolicy_vndr [3/4]'
-# SEpolicy
-rm -rf device/qcom/sepolicy_vndr
-if [ ! -d "device/qcom/sepolicy_vndr" ]; then
-    mkdir -p device/qcom/sepolicy_vndr
-    git clone https://github.com/LineageOS/android_device_qcom_sepolicy_vndr.git -b lineage-21 device/qcom/sepolicy_vndr
-fi
-
-echo 'Cloning AOSP Clang 17.0.4 [4/4]' 
-# AOSP Clang 17.0.4 
-rm -rf prebuilts/clang/host/linux-x86/r498229b 
-if [ ! -d "prebuilts/clang/host/linux-x86/r498229b" ]; then 
+echo 'Cloning Proton-Clang [4/3]' 
+# Proton-Clang
+rm -rf prebuilts/clang/host/linux-x86/proton
+if [ ! -d "prebuilts/clang/host/linux-x86/proton" ]; then 
     mkdir -p prebuilts/clang/host/linux-x86/r498229b 
-    git clone https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git -b 13.0 prebuilts/clang/host/linux-x86/r498229b
+    git clone https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/proton
 fi
 
 echo 'Completed, Now proceeding to lunch'
