26fd42c43111948dd692d1ad83decd05fe5b70b3
LeVin <133874347+TuXarr@users.noreply.github.com>
2024-06-04 13:09:18 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@ echo 'Cloning RMX1971 Vendor tree [1/3]'
 rm -rf vendor/realme/RMX1971
 if [ ! -d "vendor/realme/RMX1971" ]; then
     mkdir -p vendor/realme/RMX1971
-    git clone https://gitlab.com/kaderbava/vendor_realme_RMX1971 -b 14 vendor/realme/RMX1971
+    git clone https://github.com/TuXarr/vendor_realme_RMX1971.git -b main vendor/realme/RMX1971
 fi
 
 echo 'Cloning Kernel tree [2/3]'
@@ -12,15 +12,15 @@ echo 'Cloning Kernel tree [2/3]'
 rm -rf kernel/realme/sdm710
 if [ ! -d "kernel/realme/sdm710" ]; then
     mkdir -p kernel/realme/sdm710
-    git clone https://github.com/kaderbava/android_kernel_realme_sdm710.git -b 14-r5p kernel/realme/sdm710
+    git clone https://github.com/TuXarr/android_kernel_realme_sdm710.git -b 14-r5p kernel/realme/sdm710
 fi
 
 echo 'Cloning Proton-Clang [4/3]' 
 # Proton-Clang
-rm -rf prebuilts/clang/host/linux-x86/proton
-if [ ! -d "prebuilts/clang/host/linux-x86/proton" ]; then 
+rm -rf prebuilts/clang/host/linux-x86/r498229b
+if [ ! -d "prebuilts/clang/host/linux-x86/r498229b" ]; then 
     mkdir -p prebuilts/clang/host/linux-x86/r498229b 
-    git clone https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/proton
+    git clone https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git -b 13.0 prebuilts/clang/host/linux-x86/r498229b
 fi
 
 echo 'Completed, Now proceeding to lunch'
